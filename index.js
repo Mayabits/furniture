f66fe1c3aabee7a18aa590c126f53a26ac907b54
Mayabits <maylagoulue@gmail.com>
2018-07-24 19:27:49 +0200

    change img scroll

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-function init(){
-    console.log("DOM Cargado Jquery");
+var num = 1;
+$('#body').attr("src","images/Mesa Negra.png");
+
+$(window).scroll(function () { 
+  num = $(window).scrollTop() / 3;
+  
+ if( parseInt(num) === num){
+  
+      $('#img1').attr("src","images/Mesa negra"+".png");}
+  else{}
+  
+  if (num<1){
+    $('#img1').attr("src","images/Silla negra.png");
+  }
+    if (num>30){
+    $('img').attr("src","images/Mesa negra.png");
+      $("img").css("position","right");
+  }
+
+
+
+});
