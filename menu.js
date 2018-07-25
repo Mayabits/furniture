67337b78f5546264d22e0b0f5daaa8913eb273a8
Mayabits <maylagoulue@gmail.com>
2018-07-25 16:24:48 +0200

    sidenav menu

---
+++ menu.js
@@ -0,0 +1,20 @@
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+      var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show')) {
+          myDropdown.classList.remove('show');
+        }
+    }
+  }
